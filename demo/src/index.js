21d98a0186d46f185ca276834013a2ad9e09eef1
Nathan <playcracktheskyy@gmail.com>
2018-01-20 01:25:27 +0000

    added Resource list and updated readme

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,13 +1,30 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
+import {TestRead} from 'fhir-smartr'
 
-import Example from '../../src'
+class PatientResource extends Component {
+  
+  render() {
+    const patient = this.props.resource;
+    const name = patient.name[0];
+    const address = patient.address[0];
+    return(
+      <div>
+        <h2>{ name.given[0] + ' ' + name.family }</h2>
+        <div>{ address.line[0] }</div>
+      </div>
+    )
+  }
+  
+}
 
 class Demo extends Component {
   render() {
-    return <div>
-      <h1>fhir-smartr Demo</h1>
-    </div>
+    return (
+      <TestRead query={{ type: 'Patient', id: '0c458610-3570-4103-9263-ab84fbff6f0c'}}>
+        <PatientResource />
+      </TestRead>
+    )
   }
 }
 
